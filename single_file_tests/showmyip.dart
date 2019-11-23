c78ac7b24fd952628abb8f4f88733de313a608e4
Hatter Jiang <jht5945@gmail.com>
2019-11-23 21:36:01 +0800

    var -> final

--- single_file_tests/showmyip.dart
+++ single_file_tests/showmyip.dart
@@ -4,13 +4,13 @@ import 'dart:io';
 import 'dart:convert';
 
 main() async {
-  var url = Uri.parse('https://hatter.ink/ip/ip.jsonp');
-  var httpClient = new HttpClient();
+  final url = Uri.parse('https://hatter.ink/ip/ip.jsonp');
+  final httpClient = new HttpClient();
   httpClient.connectionTimeout = const Duration(seconds: 6);
-  var request = await httpClient.getUrl(url);
-  var response = await request.close();
-  var data = await response.transform(utf8.decoder).toList();
-  var body = data.join('');
+  final request = await httpClient.getUrl(url);
+  final response = await request.close();
+  final data = await response.transform(utf8.decoder).toList();
+  final body = data.join('');
   print(body);
   httpClient.close();
 }
